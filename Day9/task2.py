cdc65ca2730ff3ff8a33f54f1ada3202708339b5
T33py <t33py@outlook.com>
2022-12-22 19:11:58 +0100

    day9 todo

--- Day9/task2.py
+++ Day9/task2.py
@@ -1,20 +1,50 @@
 import sys
 import io
 
+rope = []
+rope_length = 10
+global head
+global tail
+
 
 def main():
-    f_in = 'ex1.txt'
+    f_in = 'task1.txt'
     raw_input = read_input(f_in)
     split = split_input(raw_input)
     input = format_input(split)
+    print(input)
 
-    print(split)
+    for i in range(rope_length):
+        rope.append((0,0))
+    
+    
+
+    all_tail_places = {}
+    unique_tail_places = 0
+    for move in input:
+        tail_places = move_rope(move)
+        for place in tail_places:
+            if place in all_tail_places:
+                all_tail_places[place] += 1
+            else:
+                all_tail_places[place] = 1
+                unique_tail_places += 1
+
+    print(all_tail_places)
+    print(unique_tail_places)
     return
 
+
+def move_rope(move):
+    global head
+    return tail_places
 
 def format_input(input):
-
-    return
+    moves = []
+    for move in input:
+        split = move.split(' ')
+        moves.append((split[0], int(split[1])))
+    return moves
 
 def split_input(input:str):
     split = input.split('\n')
